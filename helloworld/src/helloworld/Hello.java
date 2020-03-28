c1dc04f13e3a5e69e28e0034df49ab66e192f9a6
mengzi <mengzi@windows10.microdone.cn>
2020-03-28 13:41:14 +0800

    mengxiaoliang

--- helloworld/src/helloworld/Hello.java
+++ helloworld/src/helloworld/Hello.java
@@ -3,6 +3,6 @@ package helloworld;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world mengxiaoliang");
 	}
 }
